215fd47f3e0ee15e18fc431d6ab20b1812fd2f96
realrentao <realrentao@users.noreply.github.com>
2026-09-08 16:06:11 +0800

    feat(生活设施): 新增 Partie4 在理发店(gid33, 3节: 头发情况/发型/美发)，76条目/152音频(14599-14674)，含OCR清洗(ã→ɑ̃/断词/错字)

---
+++ data/sec/33.js
@@ -0,0 +1,723 @@
+window.BOOK_DATA = window.BOOK_DATA || {};
+window.BOOK_DATA[33] = {
+  "gid": 33,
+  "no": "4",
+  "name": "在理发店",
+  "gname": "生活设施",
+  "secs": [
+    {
+      "no": 1,
+      "name": "头发情况",
+      "w": [
+        [
+          "头发",
+          "cheveux",
+          "m.",
+          "fr/14599.mp3",
+          "zh/14599.mp3",
+          "tóu fā",
+          "[ʃ(ə)vø]"
+        ],
+        [
+          "分叉",
+          "bifurcation",
+          "f.",
+          "fr/14600.mp3",
+          "zh/14600.mp3",
+          "fēn chā",
+          "[bifyrka(ə)sjɔ̃]"
+        ],
+        [
+          "头皮屑",
+          "pellicule",
+          "f.",
+          "fr/14601.mp3",
+          "zh/14601.mp3",
+          "tóu pí xiè",
+          "[pe(əl)likyl]"
+        ],
+        [
+          "长的",
+          "longue",
+          "adj.",
+          "fr/14602.mp3",
+          "zh/14602.mp3",
+          "zhǎng de",
+          "[lɔ̃ːg]"
+        ],
+        [
+          "短的",
+          "court, e",
+          "adj.",
+          "fr/14603.mp3",
+          "zh/14603.mp3",
+          "duǎn de",
+          "[kur, t]"
+        ],
+        [
+          "秃的",
+          "chauve",
+          "adj.",
+          "fr/14604.mp3",
+          "zh/14604.mp3",
+          "tū de",
+          "[ʃoːv]"
+        ],
+        [
+          "光秃的",
+          "pelé, e",
+          "adj.",
+          "fr/14605.mp3",
+          "zh/14605.mp3",
+          "guāng tū de",
+          "[pale]"
+        ],
+        [
+          "油性的",
+          "huileux, se",
+          "adj.",
+          "fr/14606.mp3",
+          "zh/14606.mp3",
+          "yóu xìng de",
+          "[ɥilø, z]"
+        ],
+        [
+          "一缕毛发",
+          "un filet de cheveux",
+          "m.",
+          "fr/14607.mp3",
+          "zh/14607.mp3",
+          "yì lǚ máo fā",
+          "[œ̃ file d(ə) ʃ(ə)vø]"
+        ],
+        [
+          "弄乱",
+          "bouleverser",
+          "v.t.",
+          "fr/14608.mp3",
+          "zh/14608.mp3",
+          "nòng luàn",
+          "[bulverse]"
+        ],
+        [
+          "蓬乱的",
+          "échevelé, e",
+          "adj.",
+          "fr/14609.mp3",
+          "zh/14609.mp3",
+          "péng luàn de",
+          "[eʃɑ̃vle]"
+        ],
+        [
+          "混乱",
+          "désordre",
+          "m.",
+          "fr/14610.mp3",
+          "zh/14610.mp3",
+          "hùn luàn",
+          "[dezɔ̃rdr]"
+        ],
+        [
+          "假发",
+          "perruque",
+          "f.",
+          "fr/14611.mp3",
+          "zh/14611.mp3",
+          "jiǎ fā",
+          "[pe(ə)ryk]"
+        ],
+        [
+          "假发",
+          "postiche",
+          "m.",
+          "fr/14612.mp3",
+          "zh/14612.mp3",
+          "jiǎ fā",
+          "[pɔstiʃ]"
+        ],
+        [
+          "卷发",
+          "frisure",
+          "f.",
+          "fr/14613.mp3",
+          "zh/14613.mp3",
+          "juǎn fā",
+          "[frizyːr]"
+        ],
+        [
+          "直的",
+          "droit, e",
+          "adj.",
+          "fr/14614.mp3",
+          "zh/14614.mp3",
+          "zhí de",
+          "[drwa, at]"
+        ],
+        [
+          "干燥的",
+          "sec",
+          "adj.",
+          "fr/14615.mp3",
+          "zh/14615.mp3",
+          "gàn zào de",
+          "[sɛk]"
+        ],
+        [
+          "稀疏的头发",
+          "cheveux rares",
+          "m.",
+          "fr/14616.mp3",
+          "zh/14616.mp3",
+          "xī shū de tóu fā",
+          "[ʃ(ə)vø rar]"
+        ]
+      ],
+      "s": [
+        [
+          "Je monte dans leurs Corps et pose la main sur les cheveux, les coups de pied. Jusqu'à ce que j'atteigne la destination.",
+          "我骑在他们的身上，扯住头发，踢他们。直到我到达目的地。",
+          "《绝望的主妇》",
+          "fr/14617.mp3",
+          "zh/14617.mp3",
+          "wǒ qí zài tā men de shēn shàng ， chě zhù tóu fā ， tī tā men 。 zhí dào wǒ dào dá mù de dì 。",
+          ""
+        ],
+        [
+          "Cette fois, je sais que tu es un parasite, je suis aussi inquiet, mais je vous crois, ces choses-là va bouleverser le passé, j'espère que vous êtes heureux tous les jours!",
+          "这段时间我知道你很烦，我也很担心，但我相信你，这些不开心的事一定会过去的，希望你天天开心！",
+          "",
+          "fr/14618.mp3",
+          "zh/14618.mp3",
+          "zhè duàn shí jiān wǒ zhī dào nǐ hěn fán ， wǒ yě hěn dān xīn ， dàn wǒ xiāng xìn nǐ ， zhè xiē bù kāi xīn de shì yí dìng hùi guò qù de ， xī wàng nǐ tiān tiān kāi xīn ！",
+          ""
+        ],
+        [
+          "Dans l'est de la France, le climat est continental, il fait froid en hiver, chaud et sec en été.",
+          "在法国的东部是大陆性气候，冬季寒冷，夏季炎热而干燥。",
+          "",
+          "fr/14619.mp3",
+          "zh/14619.mp3",
+          "zài fǎ guó de dōng bù shì dà lù xìng qì hòu ， dōng jì hán lěng ， xià jì yán rè ér gàn zào 。",
+          ""
+        ]
+      ],
+      "e": [
+        [
+          "目的地",
+          "destination",
+          "n.f.",
+          "fr/14620.mp3",
+          "zh/14620.mp3",
+          "mù de dì",
+          "[destina(ə)sjɔ̃]"
+        ],
+        [
+          "多余的",
+          "parasite",
+          "adj.",
+          "fr/14621.mp3",
+          "zh/14621.mp3",
+          "duō yú de",
+          "[parazit]"
+        ],
+        [
+          "气候",
+          "climat",
+          "n.m.",
+          "fr/14622.mp3",
+          "zh/14622.mp3",
+          "qì hòu",
+          "[klima]"
+        ],
+        [
+          "大陆性的",
+          "continental, e",
+          "adj.",
+          "fr/14623.mp3",
+          "zh/14623.mp3",
+          "dà lù xìng de",
+          "[kɔ̃tinɑ̃tal]"
+        ]
+      ]
+    },
+    {
+      "no": 2,
+      "name": "发型",
+      "w": [
+        [
+          "理发店",
+          "salon de coiffure",
+          "m.",
+          "fr/14624.mp3",
+          "zh/14624.mp3",
+          "lǐ fā diàn",
+          "[salɔ̃ d(ə) kwafyːr]"
+        ],
+        [
+          "理发师",
+          "artiste capillaire",
+          "",
+          "fr/14625.mp3",
+          "zh/14625.mp3",
+          "lǐ fā shī",
+          "[artist kapil(l)εːr]"
+        ],
+        [
+          "理发师",
+          "barbier",
+          "m.",
+          "fr/14626.mp3",
+          "zh/14626.mp3",
+          "lǐ fā shī",
+          "[barbje]"
+        ],
+        [
+          "理发",
+          "se faire couper les cheveux",
+          "v.t.",
+          "fr/14627.mp3",
+          "zh/14627.mp3",
+          "lǐ fā",
+          "[s(ə) fεːr kupe lə ʃ(ə)vø]"
+        ],
+        [
+          "发型",
+          "coiffure",
+          "f.",
+          "fr/14628.mp3",
+          "zh/14628.mp3",
+          "fā xíng",
+          "[kwafyːr]"
+        ],
+        [
+          "发型",
+          "coupe",
+          "f.",
+          "fr/14629.mp3",
+          "zh/14629.mp3",
+          "fā xíng",
+          "[kup]"
+        ],
+        [
+          "理发",
+          "coupe de cheveux",
+          "f.",
+          "fr/14630.mp3",
+          "zh/14630.mp3",
+          "lǐ fā",
+          "[kup də ʃ(ə)vø]"
+        ],
+        [
+          "理发",
+          "coiffer",
+          "v.t.",
+          "fr/14631.mp3",
+          "zh/14631.mp3",
+          "lǐ fā",
+          "[kwafe]"
+        ],
+        [
+          "修剪",
+          "tailler",
+          "v.t.",
+          "fr/14632.mp3",
+          "zh/14632.mp3",
+          "xiū jiǎn",
+          "[ta(a)je]"
+        ],
+        [
+          "平头",
+          "tête plate",
+          "f.",
+          "fr/14633.mp3",
+          "zh/14633.mp3",
+          "píng tóu",
+          "[tet plat]"
+        ],
+        [
+          "刘海儿",
+          "frange",
+          "f.",
+          "fr/14634.mp3",
+          "zh/14634.mp3",
+          "liú hǎi ér",
+          "[frɑ̃ːʒ]"
+        ],
+        [
+          "剪",
+          "tondre",
+          "v.t.",
+          "fr/14635.mp3",
+          "zh/14635.mp3",
+          "jiǎn",
+          "[tɔ̃ːdr]"
+        ],
+        [
+          "卷曲的",
+          "frisé, e",
+          "adj.",
+          "fr/14636.mp3",
+          "zh/14636.mp3",
+          "juǎn qū de",
+          "[frize]"
+        ],
+        [
+          "烫发",
+          "friser les cheveux",
+          "v.t.",
+          "fr/14637.mp3",
+          "zh/14637.mp3",
+          "tàng fā",
+          "[frize lə ʃ(ə)vø]"
+        ],
+        [
+          "分开",
+          "division",
+          "f.",
+          "fr/14638.mp3",
+          "zh/14638.mp3",
+          "fēn kāi",
+          "[divizjɔ̃]"
+        ],
+        [
+          "分开",
+          "séparer",
+          "v.t.",
+          "fr/14639.mp3",
+          "zh/14639.mp3",
+          "fēn kāi",
+          "[separe]"
+        ],
+        [
+          "边",
+          "ourlet",
+          "m.",
+          "fr/14640.mp3",
+          "zh/14640.mp3",
+          "biān",
+          "[urlε]"
+        ],
+        [
+          "中央",
+          "milieu",
+          "m.",
+          "fr/14641.mp3",
+          "zh/14641.mp3",
+          "zhōng yāng",
+          "[miljø]"
+        ]
+      ],
+      "s": [
+        [
+          "Je vis assis, tel qu'un ange aux mains d'un barbier.",
+          "我就这样坐着，像一个理发师手下的天使。",
+          "",
+          "fr/14642.mp3",
+          "zh/14642.mp3",
+          "wǒ jiù zhè yàng zuò zhe ， xiàng yí gè lǐ fā shī shǒu xià de tiān shǐ 。",
+          ""
+        ],
+        [
+          "Je pense qu'elle aimait la coiffure.",
+          "我想她喜欢这个发型。",
+          "《加菲猫2》",
+          "fr/14643.mp3",
+          "zh/14643.mp3",
+          "wǒ xiǎng tā xǐ huān zhè gè fā xíng 。",
+          ""
+        ],
+        [
+          "Que puis-je faire pour vous? Permanente, manucure?",
+          "能为你做什么？烫发，美甲？",
+          "《丑女贝蒂》",
+          "fr/14644.mp3",
+          "zh/14644.mp3",
+          "néng wèi nǐ zuò shén me ？ tàng fā ， měi jiǎ ？",
+          ""
+        ],
+        [
+          "Vous êtes bien coiffée. On dirait que vous êtes allée chez le coiffeur.",
+          "您头发梳得很好，您好像去过理发店了。",
+          "",
+          "fr/14645.mp3",
+          "zh/14645.mp3",
+          "nín tóu fā shū dé hěn hǎo ， nín hǎo xiàng qù guò lǐ fā diàn le 。",
+          ""
+        ],
+        [
+          "La frange se porte sur un côté pour créer un look féminin et glamour.",
+          "刘海儿梳到一边来增加女人味和魅力。",
+          "",
+          "fr/14646.mp3",
+          "zh/14646.mp3",
+          "liú hǎi ér shū dào yì biān lái zēng jiā nǚ rén wèi hé mèi lì 。",
+          ""
+        ]
+      ],
+      "e": [
+        [
+          "指甲护理",
+          "manucure",
+          "n.f.",
+          "fr/14647.mp3",
+          "zh/14647.mp3",
+          "zhǐ jiǎ hù lǐ",
+          "[manykyre]"
+        ],
+        [
+          "给梳头",
+          "coiffé",
+          "v.t.",
+          "fr/14648.mp3",
+          "zh/14648.mp3",
+          "gěi shū tóu",
+          "[kwafe]"
+        ],
+        [
+          "魅力",
+          "glamour",
+          "n.m.",
+          "fr/14649.mp3",
+          "zh/14649.mp3",
+          "mèi lì",
+          "[glæma]"
+        ]
+      ]
+    },
+    {
+      "no": 3,
+      "name": "美发",
+      "w": [
+        [
+          "洗发水",
+          "shampooing",
+          "m.",
+          "fr/14650.mp3",
+          "zh/14650.mp3",
+          "xǐ fā shǔi",
+          "[ʃɑ̃pwɛ̃]"
+        ],
+        [
+          "头发养护",
+          "soins capillaires",
+          "m.",
+          "fr/14651.mp3",
+          "zh/14651.mp3",
+          "tóu fā yǎng hù",
+          "[swɛ̃ kapil(l)ɛːr]"
+        ],
+        [
+          "发夹",
+          "épingle à cheveux",
+          "f.",
+          "fr/14652.mp3",
+          "zh/14652.mp3",
+          "fā jiā",
+          "[epɛ̃ːgl a ʃ(ə)vø]"
+        ],
+        [
+          "卷起",
+          "retroussement",
+          "m.",
+          "fr/14653.mp3",
+          "zh/14653.mp3",
+          "juǎn qǐ",
+          "[r(ə)trusmɑ̃]"
+        ],
+        [
+          "乳胶",
+          "émulsion",
+          "f.",
+          "fr/14654.mp3",
+          "zh/14654.mp3",
+          "rǔ jiāo",
+          "[emylsjɔ̃]"
+        ],
+        [
+          "吹风机",
+          "séchoir à cheveux",
+          "m.",
+          "fr/14655.mp3",
+          "zh/14655.mp3",
+          "chūi fēng jī",
+          "[seʃwaːr a ʃ(ə)vø]"
+        ],
+        [
+          "吹风机",
+          "sèche-cheveux",
+          "m.inv.",
+          "fr/14656.mp3",
+          "zh/14656.mp3",
+          "chūi fēng jī",
+          "[seʃʃavø]"
+        ],
+        [
+          "护发素",
+          "conditionneur",
+          "m.",
+          "fr/14657.mp3",
+          "zh/14657.mp3",
+          "hù fā sù",
+          "[kɔ̃disjɔ̃nœr]"
+        ],
+        [
+          "发蜡",
+          "pommade",
+          "f.",
+          "fr/14658.mp3",
+          "zh/14658.mp3",
+          "fā là",
+          "[pɔmad]"
+        ],
+        [
+          "护发用品",
+          "articles pour conditionner les cheveux",
+          "",
+          "fr/14659.mp3",
+          "zh/14659.mp3",
+          "hù fā yòng pǐn",
+          "[artikl pur kɔ̃disjɔ̃n le ʃ(ə)vø]"
+        ],
+        [
+          "夹子",
+          "pince",
+          "f.",
+          "fr/14660.mp3",
+          "zh/14660.mp3",
+          "jiā zi",
+          "[pɛ̃ːs]"
+        ],
+        [
+          "软化",
+          "amollir",
+          "v.t.",
+          "fr/14661.mp3",
+          "zh/14661.mp3",
+          "ruǎn huà",
+          "[amɔliːr]"
+        ],
+        [
+          "定型摩丝",
+          "mousse coiffante",
+          "f.",
+          "fr/14662.mp3",
+          "zh/14662.mp3",
+          "dìng xíng mó sī",
+          "[mus kwafɑ̃t]"
+        ],
+        [
+          "染色",
+          "coloration",
+          "f.",
+          "fr/14663.mp3",
+          "zh/14663.mp3",
+          "rǎn sè",
+          "[kɔlɔrasjɔ̃]"
+        ],
+        [
+          "剪刀",
+          "ciseaux",
+          "m.",
+          "fr/14664.mp3",
+          "zh/14664.mp3",
+          "jiǎn dāo",
+          "[sizo]"
+        ],
+        [
+          "冲洗",
+          "rincer",
+          "v.t.",
+          "fr/14665.mp3",
+          "zh/14665.mp3",
+          "chōng xǐ",
+          "[rɛ̃se]"
+        ],
+        [
+          "梳理",
+          "démêler",
+          "v.t.",
+          "fr/14666.mp3",
+          "zh/14666.mp3",
+          "shū lǐ",
+          "[deme(ɛ)le]"
+        ]
+      ],
+      "s": [
+        [
+          "Décidez-vous un shampooing pour cheveux gras ou secs? Ou antipelliculaire?",
+          "您想要洗油性头发还是洗干性头发的洗发水？或者用去头屑的洗发水？",
+          "",
+          "fr/14667.mp3",
+          "zh/14667.mp3",
+          "nín xiǎng yào xǐ yóu xìng tóu fā hái shì xǐ gàn xìng tóu fā de xǐ fā shǔi ？ huò zhě yòng qù tóu xiè de xǐ fā shǔi ？",
+          ""
+        ],
+        [
+          "Dites-moi que vous utilisez un conditionneur?",
+          "告诉我你用了护发素吗？",
+          "",
+          "fr/14668.mp3",
+          "zh/14668.mp3",
+          "gào sù wǒ nǐ yòng le hù fā sù ma ？",
+          ""
+        ],
+        [
+          "C'est une bonne idée d'apporter des ciseaux à ongles.",
+          "带上指甲钳是一个好主意。",
+          "",
+          "fr/14669.mp3",
+          "zh/14669.mp3",
+          "dài shàng zhǐ jiǎ qián shì yí gè hǎo zhǔ yì 。",
+          ""
+        ],
+        [
+          "Avant d'entrer dans le palais principal pour prier, il faut laver les mains et rincer la bouche! Voilà, je respecte bien la tradition.",
+          "在进入正殿祈福之前需要洗手和漱口，既然入乡就要随俗嘛。",
+          "",
+          "fr/14670.mp3",
+          "zh/14670.mp3",
+          "zài jìn rù zhèng diàn qí fú zhī qián xū yào xǐ shǒu hé shù kǒu ， jì rán rù xiāng jiù yào súi sú ma 。",
+          ""
+        ],
+        [
+          "Démêlons le vrai du faux!",
+          "要分清楚真假！",
+          "",
+          "fr/14671.mp3",
+          "zh/14671.mp3",
+          "yào fēn qīng chǔ zhēn jiǎ ！",
+          ""
+        ]
+      ],
+      "e": [
+        [
+          "去头皮屑的",
+          "antipelliculaire",
+          "adj.",
+          "fr/14672.mp3",
+          "zh/14672.mp3",
+          "qù tóu pí xiè de",
+          "[ɑ̃tipelikyler]"
+        ],
+        [
+          "宫殿",
+          "palais",
+          "n.m.",
+          "fr/14673.mp3",
+          "zh/14673.mp3",
+          "gōng diàn",
+          "[palɛ]"
+        ],
+        [
+          "祈福，祷告",
+          "prier",
+          "v.i.",
+          "fr/14674.mp3",
+          "zh/14674.mp3",
+          "qí fú ， dǎo gào",
+          "[prije]"
+        ]
+      ]
+    }
+  ]
+};
